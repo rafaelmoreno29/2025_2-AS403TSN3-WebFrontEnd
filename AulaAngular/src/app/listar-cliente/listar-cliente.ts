0ede424ad1c831cb7acda3e1d493843ec62dad52
Rafael Moreno <rafaelmorenosax@gmail.com>
2025-08-25 22:18:17 -0300

    Aula 2

---
+++ AulaAngular/src/app/listar-cliente/listar-cliente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-cliente',
+  standalone: false,
+  templateUrl: './listar-cliente.html',
+  styleUrl: './listar-cliente.css'
+})
+export class ListarCliente {
+
+}
